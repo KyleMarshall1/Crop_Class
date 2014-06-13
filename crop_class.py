cf82cce2c863be43a8afce6cd321a34851fe93f1
KyleMarshall1 <30829@longroad.ac.uk>
2014-06-13 10:26:46 +0100

    Crop class start

---
+++ crop_class.py
@@ -0,0 +1,25 @@
+class Crop:
+    """A generic food crop"""
+
+    #constructor method
+    def __init__ (self, growth_rate, light_need, water_need):
+        #Attributes
+        self._growth = 0
+        self._days_growing = 0
+        self._growth_rate = growth_rate
+        self._light_need = light_need
+        self._water_need = water_need
+        self._status = "Seed"
+        self._type = "Generic"
+    
+def main():
+    #instantiate class
+    new_crop = Crop(1,4,3)
+    #test
+    print(new_crop._status)
+    print(new_crop._light_need)
+    print(new_crop._water_need)
+
+if __name__ == "__main__":
+    main()
+    
